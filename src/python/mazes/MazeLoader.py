fcb3781ebd739ea1fc6da05c7563d6218063d64b
Maura Thompson <thomaura@gmail.com>
2021-11-20 15:35:59 -0500

    Debugging generated100.maze, added repr to MazeSquare class

--- src/python/mazes/MazeLoader.py
+++ src/python/mazes/MazeLoader.py
@@ -13,45 +13,41 @@ from collections import deque
 
 # Mock submission that will return valid path for simple.maze
 class ReferenceMazeRunner:
-    def __init__(self):
-        # dictionary of all node names paired with the corresponding node object
-        self.master_list = {}
+    # def __init__(self):
+    #     # dictionary of all node names paired with the corresponding node object
+    #     self.master_list = {}
         
-        try:
-            with open(sys.argv[1], 'r') as f:
-                cell_nums = int(f.readline())
-                for _ in range(cell_nums):
-                    curr_line = f.readline()
-                    parts = curr_line.split(' ', 2)
-                    name = parts[0]
-                    # print(f"we're in ReferenceMazeRunner init function. name = {name}")
-                    # Strip trailing new line characters
-                    name.replace("\n", "")
-                    # name = name.strip("\n")
-                    if name not in self.master_list:
-                        # print(f"name = {name}")
-                        self.master_list[name] = MazeSquare(name)
-                    square = self.master_list.get(name)
-                    exits = parts[1].split(',')
-                    for exit in exits:
-                        direction, next_square = exit.split(':')
-                        if next_square not in self.master_list:
-                            self.master_list[next_square] = MazeSquare(next_square)
-                        square.add_exit(self.master_list.get(next_square), direction)
-                start, end = f.readline().split(' ')
-                # print(self.master_list)
-                # print(self.master_list.keys())
-                # print(len(self.master_list.keys()))
+    #     try:
+    #         with open(sys.argv[1], 'r') as f:
+    #             cell_nums = int(f.readline())
+    #             for _ in range(cell_nums):
+    #                 curr_line = f.readline()
+    #                 parts = curr_line.split(' ', 2)
+    #                 name = parts[0]
+    #                 # print(f"we're in ReferenceMazeRunner init function. name = {name}")
+    #                 # Strip trailing new line characters
+    #                 name.replace("\n", "")
+    #                 # name = name.strip("\n")
+    #                 if name not in self.master_list:
+    #                     # print(f"name = {name}")
+    #                     self.master_list[name] = MazeSquare(name)
+    #                 square = self.master_list.get(name)
+    #                 exits = parts[1].split(',')
+    #                 for exit in exits:
+    #                     direction, next_square = exit.split(':')
+    #                     if next_square not in self.master_list:
+    #                         self.master_list[next_square] = MazeSquare(next_square)
+    #                     square.add_exit(self.master_list.get(next_square), direction)
+    #             start, end = f.readline().split(' ')
+    #             # print(self.master_list)
+    #             # print(self.master_list.keys())
+    #             # print(len(self.master_list.keys()))
 
-        except FileNotFoundError:
-            print('Location of maze file was not found')
-        except IOError:
-            print('IO Exception reading from maze file')
+    #     except FileNotFoundError:
+    #         print('Location of maze file was not found')
+    #     except IOError:
+    #         print('IO Exception reading from maze file')
 
-    def __repr__(self):
-        """Debugging-friendly representation"""
-
-        return f"<MazeSquare: {self.name}>"
 
     # def run(self, start, end):
     #     """ Use breadth-first search to check if start and end nodes
@@ -106,17 +102,17 @@ class ReferenceMazeRunner:
         seen.add(start)
         # print(f"adding, {start.name}")
 
-        for exit in start.exits:
+        for direction in start.exits:
         # for exit in start.exits.values():
             # print(exit)
-            exit_object = start.get_square(exit)
+            exit_object = start.get_square(direction)
             if exit_object not in seen:
-                print(exit)
-                path.append(exit)
+                print(direction)
+                path.append(direction)
                 print(
                     f"calling method on {start.name}'s exit {exit_object.name}"
                 )
-
+                print(f"{exit_object.name}'s exits are {exit_object.exits}")
                 if self.run(exit_object, end, seen, path):
                     # print(exit)
                     # path.append(exit)
@@ -137,6 +133,9 @@ class MazeLoader:
                     curr_line = f.readline()
                     parts = curr_line.split(' ', 2)
                     name = parts[0]
+                    # print(f"we're in MazeLoader init function. name = {name}")
+                    name.replace("\n", "")
+                    # name = name.strip("\n")
                     if name not in self.master_list:
                         self.master_list[name] = MazeSquare(name)
                     square = self.master_list.get(name)
@@ -147,17 +146,35 @@ class MazeLoader:
                             self.master_list[next_square] = MazeSquare(next_square)
                         square.add_exit(self.master_list.get(next_square), direction)
                 start, end = f.readline().split(' ')
+                print(f"We're in MazeLoader init function")
+                # print(self.master_list)
+                print(self.master_list.keys())
+                # print(len(self.master_list.keys()))
                 current = self.master_list.get(start)
                 # Change the implemenation of ReferenceMazeRunner, or replace it with your class here
-                runners = [ReferenceMazeRunner()]
-                for runner in runners:
-                    result = runner.run(self.master_list.get(start), self.master_list.get(end))
-                    for step in result:
-                        current = current.get_square(step)
-                        if current == None:
-                            print('Invalid path returned')
-                            break
-                    print('Returned ' + ('valid' if self.master_list.get(end).get_name() == current.get_name() else 'invalid') + ' path')
+                runner = ReferenceMazeRunner()
+                print(f"we're in MazeLoader, runner = {runner}")
+                print(f"we're in MazeLoader, self.master_list.get(start) = {self.master_list.get(start)}")
+                result = runner.run(self.master_list.get(start), self.master_list.get(end))
+                print(f"we're in MazeLoader, result = {result}")
+                for step in result:
+                    print(f"we're in MazeLoader, step = {step}")
+                    current = current.get_square(step)
+                    if current == None:
+                        print('Invalid path returned')
+                        break
+                # runners = [ReferenceMazeRunner()]
+                # print(f"we're in MazeLoader, runners = {runners}")
+                # for runner in runners:
+                #     result = runner.run(self.master_list.get(start), self.master_list.get(end))
+                #     print(f"we're in MazeLoader, result = {result}")
+                #     for step in result:
+                #         print(f"we're in MazeLoader, step = {step}")
+                #         current = current.get_square(step)
+                #         if current == None:
+                #             print('Invalid path returned')
+                #             break
+                print('Returned ' + ('valid' if self.master_list.get(end).get_name() == current.get_name() else 'invalid') + ' path')
         except FileNotFoundError:
             print('Location of maze file was not found')
         except IOError:
@@ -178,7 +195,12 @@ class MazeSquare:
         return self.exits.keys()
                     
     def get_square(self, direction):
-        return self.exits.get(direction, None)                        
+        return self.exits.get(direction, None) 
+
+    def __repr__(self):
+        """Debugging-friendly representation"""
+
+        return f"<MazeSquare: {self.name}>"                       
 
 MazeLoader()
 
